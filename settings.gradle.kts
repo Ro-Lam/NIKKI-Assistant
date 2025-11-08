4bd0e3cbe965e5c46caf0fee1f5147ccdf6a81fb
Ro-Lam <messererokkilampone@gmail.com>
2025-11-08 19:57:01 +0800

    Add files via upload

---
+++ settings.gradle.kts
@@ -0,0 +1,17 @@
+﻿pluginManagement {
+    repositories {
+        google()
+        mavenCentral()
+        gradlePluginPortal()
+    }
+}
+dependencyResolutionManagement {
+    repositoriesMode.set(RepositoriesMode.FAIL_ON_PROJECT_REPOS)
+    repositories {
+        google()
+        mavenCentral()
+        maven { url = uri("https://jitpack.io") }
+    }
+}
+rootProject.name = "NIKKI-Assistant"
+include(":app")
